adc5c4862623de23e2391110bd1ac1724ad0ffda
ROYNARD <michael.roynard@cea.fr>
2016-02-26 14:53:30 +0100

    Implémentation modèle intégrale/dérivées pour fonctions usuelles. Reste à faire la composition et les fonctions usuelles non triviales.

--- inc/Formal_Maths.h
+++ inc/Formal_Maths.h
@@ -15,15 +15,16 @@
 
 #include "Formal_Maths_config.h"
 
-#include <memory>
-#include <limits>
 #include <complex>
+#include <functional>
+#include <limits>
+#include <memory>
 
 namespace fm {
 
 	using uint_t = unsigned long long int;
 	using int_t = long long int;
-	using real_t = long double;
+	using real_t = double;
 	using complex_t = std::complex<real_t>;
 
 
@@ -54,10 +55,12 @@ namespace fm {
 	template<class T> bool operator<(const T& /*lhs*/, const InfinityType& inf) { return inf.sign != InfinityType::MINUS; }
 	template<class T> bool operator<=(const T& lhs, const InfinityType& inf) { return lhs > inf; }
 
+	/*
+	* Domaine
+	*/
 
 	template<class ValueType>
-	class Domain {
-	public:
+	struct Domain {
 		using value_type = ValueType;
 
 		static ValueType inf() {
@@ -77,26 +80,149 @@ namespace fm {
 
 	using C = Domain<complex_t>;
 
-	class R : Domain<real_t>{
-	public:
+	struct R : Domain<real_t>{
 		using Domain<real_t>::has;
 		static bool has(complex_t c) { return c.imag() == 0; }
 	};
 
-	class Z : Domain<int_t> {
-	public:
+	struct Z : Domain<int_t> {
 		using Domain<int_t>::has;
 		static bool has(complex_t /*c*/) { return false; }
 		static bool has(real_t /*r*/) { return false; }
 	};
 
-	class N : Domain<uint_t> {
-	public:
+	struct N : Domain<uint_t> {
 		using Domain<uint_t>::has;
 		static bool has(complex_t /*c*/) { return false; }
 		static bool has(real_t /*r*/) { return false; }
 		static bool has(int_t i) { return i >= 0; }
 	};
+
+
+	/*
+	* Usual functions impl
+	*/
+
+	template<class Func, class Ret, class ... Args> Ret exec_func(Func, Args ... args);
+	template<class Func, class Ret, class ... Args> Ret exec_deritative(Func, Args ... args);
+	template<class Func, class Ret, class ... Args> Ret exec_primitive(Func, Args ... args);
+
+	template<class ValueType>
+	struct Const { ValueType value; };
+
+	template<class ValueType>
+	struct Pow { ValueType pow; };
+
+	template<class ValueType>
+	struct Linear { };
+
+	template<class ValueType>
+	struct FracPow { ValueType fracPow; };
+
+	template<class ValueType>
+	struct Invert { };
+
+	template<class ValueType>
+	struct SquareRoot{ };
+
+	template<class ValueType>
+	struct Cos { };
+
+	template<class ValueType>
+	struct Sin { };
+	
+	template<template <class> class FunctionBase, class ValueType>
+	struct Function {
+		Function(FunctionBase<ValueType> f) : func(f) {}
+		FunctionBase<ValueType> func;
+	};
+
+	//Func exec
+
+	template<class ValueType>
+	ValueType exec_func(Const<ValueType> f, ValueType x) { return f.value; }
+
+	template<class ValueType>
+	ValueType exec_func(Linear<ValueType> f, ValueType x) { return x; }
+
+	template<class ValueType>
+	ValueType exec_func(Pow<ValueType> f, ValueType x) { return std::pow(x, f.pow); }
+
+	template<class ValueType>
+	ValueType exec_func(SquareRoot<ValueType> f, ValueType x) { return std::sqrt(x); }
+
+	template<class ValueType>
+	ValueType exec_func(Invert<ValueType> f, ValueType x) { return 1 / x; }
+
+	template<class ValueType>
+	ValueType exec_func(FracPow<ValueType> f, ValueType x) { return 1 / std::pow(x, f.fracPow); }
+
+	template<class ValueType>
+	ValueType exec_func(Cos<ValueType> f, ValueType x) { return std::cos(x); }
+
+	template<class ValueType>
+	ValueType exec_func(Sin<ValueType> f, ValueType x) { return std::sin(x); }
+
+	template<template <class> class FunctionBase, class ValueType>
+	ValueType exec_func(Function<FunctionBase, ValueType> f, ValueType x) { return exec_func(f.func, x); }
+
+	//Deritatives
+
+	template<class ValueType>
+	ValueType exec_deritative(Const<ValueType> f, ValueType x) { return 0; }
+
+	template<class ValueType>
+	ValueType exec_deritative(Linear<ValueType> f, ValueType x) { return 1; }
+
+	template<class ValueType>
+	ValueType exec_deritative(SquareRoot<ValueType> f, ValueType x) { return 1 / (2 * std::sqrt(x)); }
+
+	template<class ValueType>
+	ValueType exec_deritative(Pow<ValueType> f, ValueType x) { return f.pow * std::pow(x, f.pow - 1); }
+
+	template<class ValueType>
+	ValueType exec_deritative(Invert<ValueType> f, ValueType x) { return -1 / (x * x); }
+
+	template<class ValueType>
+	ValueType exec_deritative(FracPow<ValueType> f, ValueType x) { return -f.fracPow / std::pow(x, f.fracPow + 1); }
+
+	template<class ValueType>
+	ValueType exec_deritative(Cos<ValueType> f, ValueType x) { return -std::sin(x); }
+
+	template<class ValueType>
+	ValueType exec_deritative(Sin<ValueType> f, ValueType x) { return std::cos(x); }
+
+	template<template <class> class FunctionBase, class ValueType>
+	ValueType exec_deritative(Function<FunctionBase, ValueType> f, ValueType x) { return exec_deritative(f.func, x); }
+
+	//Primitives
+
+	template<class ValueType>
+	ValueType exec_primitive(Const<ValueType> f, ValueType x) { return f.value * x; }
+
+	template<class ValueType>
+	ValueType exec_primitive(Linear<ValueType> f, ValueType x) { return x * x * 0.5; }
+
+	template<class ValueType>
+	ValueType exec_primitive(SquareRoot<ValueType> f, ValueType x) { return (2 / 3) * std::pow(x, 1.5); }
+
+	template<class ValueType>
+	ValueType exec_primitive(Pow<ValueType> f, ValueType x) { return std::pow(x, f.pow); }
+
+	template<class ValueType>
+	ValueType exec_primitive(Invert<ValueType> f, ValueType x) { return std::log(x); }
+
+	template<class ValueType>
+	ValueType exec_primitive(FracPow<ValueType> f, ValueType x) { return -1 / ((f.fracPow - 1) * std::pow(x, f.fracPow - 1)); }
+
+	template<class ValueType>
+	ValueType exec_primitive(Cos<ValueType> f, ValueType x) { return std::sin(x); }
+
+	template<class ValueType>
+	ValueType exec_primitive(Sin<ValueType> f, ValueType x) { return -std::cos(x); }
+
+	template<template <class> class FunctionBase, class ValueType>
+	ValueType exec_primitive(Function<FunctionBase, ValueType> f, ValueType x) { return exec_primitive(f.func, x); }
 
 }
 
--- tests/src/Formal_Maths_Tests.cpp
+++ tests/src/Formal_Maths_Tests.cpp
@@ -14,6 +14,17 @@ int main(int /*argc*/, char* /*argv[]*/){
 	std::cout << R::has(r) << std::endl;
 	std::cout << N::has(int_t(-1)) << std::endl;
 
+
+	const auto const_func = Const<real_t>{ 15. };
+	const auto a = exec_func(const_func, 1155555.);
+	const auto b = exec_deritative(const_func, 13.);
+	const auto d = exec_primitive(const_func, 5.);
+
+	const auto f = Function<Const, real_t>(const_func);
+	const auto aa = exec_func(f, 1155555.);
+	const auto bb = exec_deritative(f, 13.);
+	const auto dc = exec_primitive(f, 5.);
+
 	std::cout << "Press enter to continue...";
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 

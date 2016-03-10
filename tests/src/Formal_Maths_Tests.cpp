cb922628b2e5dc0542f6a283eb38e9698a9a32c0
Michaël Roynard <michaelroynard@gmail.com>
2016-03-10 22:06:59 +0100

    Updated H2OFastTests.

--- tests/src/Formal_Maths_Tests.cpp
+++ tests/src/Formal_Maths_Tests.cpp
@@ -9,41 +9,41 @@
 using namespace fm;
 
 int main(int /*argc*/, char** /*argv*/){
-	
+    
     const auto f1 = LinearPow<real_t>::NewConst(15.);
     const auto f2 = LinearPow<real_t>::NewLinear(40.);
-	const auto f3 = f1 + f2;
-	const auto deriv_f1 = f1.deritative();
-	const auto deriv_f2 = f2.deritative();
-	const auto deriv_f3 = f3.deritative();
-	const auto prim_f1 = f1.primitive();
-	const auto prim_f2 = f2.primitive();
-	const auto prim_f3 = f3.primitive();
+    const auto f3 = f1 + f2;
+    const auto deriv_f1 = f1.deritative();
+    const auto deriv_f2 = f2.deritative();
+    const auto deriv_f3 = f3.deritative();
+    const auto prim_f1 = f1.primitive();
+    const auto prim_f2 = f2.primitive();
+    const auto prim_f3 = f3.primitive();
 
-	const auto a = f1(1.);
-	const auto b = deriv_f1(15.);
-	const auto c = prim_f1(15.);
+    const auto a = f1(1.);
+    const auto b = deriv_f1(15.);
+    const auto c = prim_f1(15.);
 
-	const auto aa = f2(1.);
-	const auto bb = deriv_f2(15.);
-	const auto cc = prim_f2(15.);
+    const auto aa = f2(1.);
+    const auto bb = deriv_f2(15.);
+    const auto cc = prim_f2(15.);
 
-	const auto aaa = f3(1.);
-	const auto bbb = deriv_f3(15.);
-	const auto ccc = prim_f3(15.);
+    const auto aaa = f3(1.);
+    const auto bbb = deriv_f3(15.);
+    const auto ccc = prim_f3(15.);
 
-	const auto f4 = f1 * f2;
-	const auto defiv_f4 = f4.deritative();
-	const auto prim_f4 = f4.primitive();
+    const auto f4 = f1 * f2;
+    const auto defiv_f4 = f4.deritative();
+    const auto prim_f4 = f4.primitive();
 
-	const auto f5 = f2 / f1;
-	const auto defiv_f5 = f5.deritative();
-	const auto prim_f5 = f5.primitive();
-	
-	std::cout << "Press enter to continue...";
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    const auto f5 = f2 / f1;
+    const auto defiv_f5 = f5.deritative();
+    const auto prim_f5 = f5.primitive();
+    
+    std::cout << "Press enter to continue...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 #pragma warning ( pop )
